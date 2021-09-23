03be1ffbcea2331f5b9705cbf12fe89fae23b0e1
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2021-09-23 09:35:29 +0530

    basic server code without mongod
    it is my server without to mongod

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const port = 5000;
 const path = require('path');
+const fs = require('fs');
 
 // for serving static files
 app.use('/static',express.static('static'));
+
+// url encoded propety for express
+app.use(express.urlencoded({extended:true}));
 
 // setting template engine as pug
 app.set('view engine','pug')
@@ -12,15 +16,26 @@ app.set('view engine','pug')
 // set the views directory
 app.set('views', path.join(__dirname,'views'));
 
-// make directory of Views with file demo.pug
+// Endpoints
 
-app.get('/demo',(req,res)=>{
+app.get('/',(req,res)=>{
     res.render('demo.pug')
 });
-// you will find the file serving on location url /demo
-app.get('/',(req,res)=>{
-    res.send('your server 2')
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    name=req.body.name
+    age=req.body.age
+    gmail=req.body.gmail
+    address=req.body.address
+    more=req.body.more
+    res.render('index.pug')
+    let outputToWrite=` his name ${name}`;
+    fs.writeFileSync('output.txt', outputToWrite);
+
 });
+
+
+// Start the server 
 app.listen(port,()=>{
     console.log("you server is running");
 })
